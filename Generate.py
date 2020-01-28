450070119b03aee8019813585908d6a0880632f3
Batuhan Tekmen <44123432+tekmen0@users.noreply.github.com>
2020-01-28 12:06:24 +0300

    Create Generate.py

---
+++ Generate.py
@@ -0,0 +1,35 @@
+"""
+Script to run generator model.
+"""
+
+#%matplotlib inline
+
+from keras.models import load_model
+import matplotlib.pyplot as plt
+import numpy as np
+
+generator = load_model('generator.h5')
+
+def show_images(noise):
+    generated_images = generator.predict(noise) 
+    plt.figure(figsize=(10,10))
+
+    for i, image in enumerate(generated_images):
+        plt.subplot(10, 10, i+1)
+        for i, image in enumerate(generated_images):
+            plt.subplot(10, 10, i+1)
+            if channels == 1:
+                plt.imshow(image.reshape((img_rows, img_cols)), cmap='gray')
+            else:
+                plt.imshow(image.reshape((img_rows, img_cols, channels)))
+        plt.axis('off')
+
+    plt.tight_layout()
+    plt.show()
+
+noise = np.random.normal(0, 1, size=(100, noise_dim))
+show_images(noise)
+
+
+
+
